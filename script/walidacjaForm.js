94be590e17d8dcbb2365d7f8c16447b975471e48
WitGes <witek_gesse@poczta.onet.pl>
2016-02-20 17:58:13 +0100

    Poprawianie kodu

--- script/walidacjaForm.js
+++ script/walidacjaForm.js
@@ -64,36 +64,5 @@ $(document).ready(
                 }
             }
         );
-
-        //$("input[type=submit]").click(
-        //    function () {
-        //        //$(this).delay(5000);
-        //        $('body').css({
-        //            opacity: 0.2,
-        //            background: '#F4EDA5',
-        //
-        //        });
-        //        //var text='Dziękujemy za wysłanie nam informacji';
-        //        //$('.message').html(text);
-        //
-        //    }
-        //);
-
-        //$("form").submit(function() {
-        //    $(".mask").css('display','block');
-        //    var timer = 5;
-        //    $('.time').html(timer);
-        //    setInterval(function() {
-        //        if(timer > 0) {
-        //            //$('.odliczanie').addClass('time');
-        //            $('.time').html(--timer).show(1000);
-        //        }
-        //        else{
-        //
-        //        }
-        //
-        //    }, 1000);
-        //
-        //});
     });
 
