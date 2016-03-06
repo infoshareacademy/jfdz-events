45328743ba0c077bd9f27e875ae1cd0b7f0e1c3e
hania_c <han.czajka@gamil.com>
2016-03-06 18:23:22 +0100

    gra zalazki

---
+++ script/gra.js
@@ -0,0 +1,28 @@
+/**
+ * Created by hania_c on 06.03.16.
+ */
+
+var board=[];
+var teraz=new Date();
+var month=teraz.getMonth();
+var year=teraz.getFullYear();
+
+function daysInMonth(y, m) {
+    return new Date(y, m, 0).getDate();
+}
+
+
+$(document).ready(function() {
+
+
+    function createBoard(size) {
+        for (var x = 0 ; x < size ; x++) {
+            board[x]= { date: new Date(year, month, x+1)};
+        }
+    }
+
+    createBoard(daysInMonth(year, month));
+
+    console.log(board);
+
+});
