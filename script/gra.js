b4a11af439593e7771c3996c2438696fa15688d5
haniac <han.czajka@gmail.com>
2016-03-13 00:54:36 +0100

    kalendarz do gry - poczatek

--- script/gra.js
+++ script/gra.js
@@ -6,6 +6,8 @@ var board=[];
 var teraz=new Date();
 var month=teraz.getMonth();
 var year=teraz.getFullYear();
+var day=teraz.getDay();
+//var firstDay=
 
 function daysInMonth(y, m) {
     return new Date(y, m, 0).getDate();
@@ -24,6 +26,12 @@ $(document).ready(function() {
     //daysInMonth(year, month);
     createBoard(daysInMonth(year, month+1));
 
+
+    for (var i = 0 ; i < board.length ; i++) {
+        $('#gierka').append('<div>' + (i+1) + '</div>');
+    }
+
     console.log(board);
+    //$('#gierka').append(board);
 
 });
